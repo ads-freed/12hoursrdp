2e39ddaef09e90293c9ede9eb79c5ccf4f52eea8
ads-freed <57936630+ads-freed@users.noreply.github.com>
2024-06-20 13:49:40 +0800

    Update autosetngrok.sh

--- autosetngrok.sh
+++ autosetngrok.sh
@@ -2,6 +2,6 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Auto Setup Ngrok Token"
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 ./ngrok authtoken 2i7ivxmwMewjkujrB3rHBoJDimp_gU2bRAg4LGJwFFyw5duF #put your authtoken, not my token >:3
